e77901508c75444740e7cb37046c06367378a21c
Ilari Suhonen <ilari.suhonen@gmail.com>
2025-09-05 17:06:32 +0300

    feat(parser): application without space between expressions
    Signed-off-by: Ilari Suhonen <ilari.suhonen@gmail.com>

--- src/main/java/blue/lhf/lambda_calculus/parser/Parser.java
+++ src/main/java/blue/lhf/lambda_calculus/parser/Parser.java
@@ -6,12 +6,12 @@ import blue.lhf.lambda_calculus.model.Expression;
 import blue.lhf.lambda_calculus.model.Variable;
 import blue.lhf.lambda_calculus.parser.tokeniser.Token;
 import blue.lhf.lambda_calculus.parser.tokeniser.Tokenizer;
+import blue.lhf.lambda_calculus.parser.tokeniser.Tokenizer.Lambda;
+import blue.lhf.lambda_calculus.parser.tokeniser.Tokenizer.LeftParen;
+import blue.lhf.lambda_calculus.parser.tokeniser.Tokenizer.Space;
+import blue.lhf.lambda_calculus.parser.tokeniser.Tokenizer.VariableToken;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.ListIterator;
-import java.util.Map;
+import java.util.*;
 
 public class Parser {
 	private static boolean skipToNextSubExpression(ListIterator<Token> tokens) {
@@ -42,114 +42,114 @@ public class Parser {
 		return expr;
 	}
 
-    private Expression parseExpression(final ListIterator<Token> tokens, final Map<String, Variable> boundVariables) {
-        if (!tokens.hasNext()) return null;
-        final List<Expression> expressions = new ArrayList<>();
-        do {
-            switch (tokens.next()) {
-                case Tokenizer.LeftParen ignored -> {
-                    final Expression inner = parseExpression(tokens, boundVariables);
-                    if (!tokens.hasNext() || !(tokens.next() instanceof Tokenizer.RightParen)) {
-                        return null;
-                    }
-                    expressions.add(inner);
-                }
-                case Tokenizer.Lambda ignored -> {
-                    final List<Tokenizer.VariableToken> variableTokens = new ArrayList<>();
-                    while (tokens.hasNext() && (tokens.next() instanceof Tokenizer.VariableToken v)) {
-                        variableTokens.add(v);
-                    }
-                    tokens.previous();
-                    if (!tokens.hasNext() || !(tokens.next() instanceof Tokenizer.Dot)) {
-                        tokens.previous();
-                        return null;
-                    }
-                    final Map<String, Variable> newBoundVariables = new HashMap<>(Map.copyOf(boundVariables));
-                    final List<Variable> lambdaArguments = new ArrayList<>();
-                    for (Tokenizer.VariableToken variableToken : variableTokens) {
-                        final String variableName = variableToken.getText();
+	private Expression parseExpression(final ListIterator<Token> tokens, final Map<String, Variable> boundVariables) {
+		if (!tokens.hasNext()) return null;
+		final List<Expression> expressions = new ArrayList<>();
+		do {
+			switch (tokens.next()) {
+				case LeftParen ignored -> {
+					final Expression inner = parseExpression(tokens, boundVariables);
+					if (!tokens.hasNext() || !(tokens.next() instanceof Tokenizer.RightParen)) {
+						return null;
+					}
+					expressions.add(inner);
+				}
+				case Lambda ignored -> {
+					final List<VariableToken> variableTokens = new ArrayList<>();
+					while (tokens.hasNext() && (tokens.next() instanceof VariableToken v)) {
+						variableTokens.add(v);
+					}
+					tokens.previous();
+					if (!tokens.hasNext() || !(tokens.next() instanceof Tokenizer.Dot)) {
+						tokens.previous();
+						return null;
+					}
+					final Map<String, Variable> newBoundVariables = new HashMap<>(Map.copyOf(boundVariables));
+					final List<Variable> lambdaArguments = new ArrayList<>();
+					for (VariableToken variableToken : variableTokens) {
+						final String variableName = variableToken.getText();
 
-                        final Variable variable = new Variable() {
-                            @Override
-                            public String toString() {
-                                return variableName;
-                            }
-                        };
+						final Variable variable = new Variable() {
+							@Override
+							public String toString() {
+								return variableName;
+							}
+						};
 
-                        lambdaArguments.add(variable);
-                        newBoundVariables.put(variableName, variable); // shadow pre-existing bound variables
-                    }
-                    final Expression body = parseExpression(tokens, newBoundVariables);
-                    if (body == null) return null;
+						lambdaArguments.add(variable);
+						newBoundVariables.put(variableName, variable); // shadow pre-existing bound variables
+					}
+					final Expression body = parseExpression(tokens, newBoundVariables);
+					if (body == null) return null;
 
-                    final ListIterator<Variable> argumentIterator = lambdaArguments.listIterator(lambdaArguments.size());
-                    Expression inner = body;
+					final ListIterator<Variable> argumentIterator = lambdaArguments.listIterator(lambdaArguments.size());
+					Expression inner = body;
 
-                    do {
-                        final Expression currentInner = inner;
-                        final Variable innerVariable = argumentIterator.previous();
-                        argumentIterator.remove();
-                        inner = new Abstraction() {
-                            @Override
-                            public Variable variable() {
-                                return innerVariable;
-                            }
+					do {
+						final Expression currentInner = inner;
+						final Variable innerVariable = argumentIterator.previous();
+						argumentIterator.remove();
+						inner = new Abstraction() {
+							@Override
+							public Variable variable() {
+								return innerVariable;
+							}
 
-                            @Override
-                            public Expression body() {
-                                return currentInner;
-                            }
-                        };
-                    } while (argumentIterator.hasPrevious());
-                    expressions.add(inner);
-                }
-                case Tokenizer.VariableToken variable -> {
-                    expressions.add(boundVariables.computeIfAbsent(variable.getText(), name -> new Variable() {
-                        @Override
-                        public String toString() {
-                            return name;
-                        }
-                    }));
-                }
-                default -> {
-                    tokens.previous();
-                    return null;
-                }
-            }
-        } while (consumeIfSpace(tokens));
+							@Override
+							public Expression body() {
+								return currentInner;
+							}
+						};
+					} while (argumentIterator.hasPrevious());
+					expressions.add(inner);
+				}
+				case VariableToken variable -> {
+					expressions.add(boundVariables.computeIfAbsent(variable.getText(), name -> new Variable() {
+						@Override
+						public String toString() {
+							return name;
+						}
+					}));
+				}
+				default -> {
+					tokens.previous();
+					return null;
+				}
+			}
+		} while (skipToNextSubExpression(tokens));
 
-        if (expressions.size() > 1) {
-            Application result = new Application() {
-                @Override
-                public Expression function() {
-                    return expressions.getFirst();
-                }
+		if (expressions.size() > 1) {
+			Application result = new Application() {
+				@Override
+				public Expression function() {
+					return expressions.getFirst();
+				}
 
-                @Override
-                public Expression argument() {
-                    return expressions.get(1);
-                }
-            };
+				@Override
+				public Expression argument() {
+					return expressions.get(1);
+				}
+			};
 
-            for (int i = 2; i < expressions.size(); i++) {
-                final Expression currentResult = result;
-                final Expression childExpression = expressions.get(i);
-                result = new Application() {
-                    @Override
-                    public Expression function() {
-                        return currentResult;
-                    }
+			for (int i = 2; i < expressions.size(); i++) {
+				final Expression currentResult = result;
+				final Expression childExpression = expressions.get(i);
+				result = new Application() {
+					@Override
+					public Expression function() {
+						return currentResult;
+					}
 
-                    @Override
-                    public Expression argument() {
-                        return childExpression;
-                    }
-                };
-            }
+					@Override
+					public Expression argument() {
+						return childExpression;
+					}
+				};
+			}
 
-            return result;
-        }
-        return expressions.getFirst();
-    }
+			return result;
+		}
+		return expressions.getFirst();
+	}
 
 }
